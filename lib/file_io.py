4c3c57e331e1b3b558c2fb3c0583a9ea4ccdb264
Prabhdip Gill <prabhdip.gill@flatironschool.com>
2022-07-31 21:31:27 -0700

    Write template file.io for main branch

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,11 +1,8 @@
 def write_file(file_name, file_content):
-    with open(f'{file_name}.txt', 'w') as f:
-        f.write(file_content)
+    pass
 
 def append_file(file_name, append_content):
-    with open(f'{file_name}.txt', 'a') as f:
-        f.write(append_content)
+    pass
 
 def read_file(file_name):
-    with open(f'{file_name}.txt') as f:
-        return f.read()
+    pass
