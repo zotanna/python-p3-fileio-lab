86c774681fde5a37681454735256dffc77d3650d
Ben Botsford <ben.botsford@flatironschool.com>
2022-10-25 15:34:47 -0500

    update tests

--- lib/testing/file_io_test.py
+++ lib/testing/file_io_test.py
@@ -3,61 +3,26 @@ from file_io import write_file, append_file, read_file
 import uuid
 import os
 
-'''Function write_files() in file_io.py'''
+
 def test_write_file():
+    '''file_io.py: write_file() takes a filename and input and writes the input to a file.'''
+    write_file('lib/testing/write.txt', 'Hello, World!')
+    f = open('lib/testing/write.txt', 'r')
+    assert(f.read() == 'Hello, World!')
+    os.remove('lib/testing/write.txt')
 
-  #Create temp directory to write file to and see if it exits.
-  with tempfile.TemporaryDirectory() as tmp_dirname:
-    filename = str(uuid.uuid4())
-    expected_file_content = str(uuid.uuid4())
-    write_file(f'{tmp_dirname}/{filename}',expected_file_content)
-    assert os.path.exists(f'{tmp_dirname}/{filename}.txt') == True
-    
-    # Check file content
-    file_content = ''
-    with open(f'{tmp_dirname}/{filename}.txt') as f:
-      file_content = f.read()
-
-      assert file_content == expected_file_content
-
-'''Function append_file() in file_io.py'''
 def test_append_to_file():
+    '''file_io.py: append_file() takes an existing filename and input and writes the input to the file.'''
+    write_file('lib/testing/append.txt', 'Hello, World!\n')
+    append_file('lib/testing/append.txt', 'How are you doing today?')
 
-  #Create temp directory to write file to and see if it exits.
-  with tempfile.TemporaryDirectory() as tmp_dirname:
-
-    filename = str(uuid.uuid4())
-    expected_file_content = f'{str(uuid.uuid4())}\n'
-    expected_append_content= str(uuid.uuid4())
-
-    write_file(f'{tmp_dirname}/{filename}', expected_file_content)
-    append_file(f'{tmp_dirname}/{filename}', expected_append_content)
-
-    assert os.path.exists(f'{tmp_dirname}/{filename}.txt') == True
-    
-    # Check file content
-    expected_content_list = [expected_file_content, expected_append_content]
-    
-    with open(f'{tmp_dirname}/{filename}.txt') as f:
-      index = 0
-
-      for line in f:
-        assert expected_content_list[index] == line
-        index += 1
-
-'''Function read_file() in file_io.py'''
+    f = open('lib/testing/append.txt', 'r')
+    assert(f.read() == 'Hello, World!\nHow are you doing today?')
+    os.remove('lib/testing/append.txt')
 
 def test_read_file():
+    '''file_io.py: read_file() takes an existing filename and reads the text from the file.'''
+    write_file('lib/testing/read.txt', 'Hello, World!')
 
-  #Create temp directory to write file to and see if it exits.
-  with tempfile.TemporaryDirectory() as tmp_dirname:
-    filename = str(uuid.uuid4())
-    expected_file_content = str(uuid.uuid4())
-
-    write_file(f'{tmp_dirname}/{filename}', expected_file_content)
-    file_content = read_file(f'{tmp_dirname}/{filename}')
-
-    assert os.path.exists(f'{tmp_dirname}/{filename}.txt') == True
-    
-    # Check file content
-    assert expected_file_content == file_content
+    text = read_file('lib/testing/read.txt')
+    assert(text == 'Hello, World!')
